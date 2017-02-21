9bed4ec378c6d8d692517b66805253d8e1620bc3
shinancao <1070528413@qq.com>
2017-02-22 00:01:39 +0800

    initial

---
+++ VoiceNotebook/Classes/Common/Model/Audio.swift
@@ -0,0 +1,15 @@
+//
+//  Audio.swift
+//  VoiceNotebook
+//
+//  Created by Shinancao on 2017/2/21.
+//  Copyright © 2017年 ZhangNan. All rights reserved.
+//
+
+import Foundation
+
+struct Audio {
+    let name: String
+    let recordDate: Date
+    let filePath: String
+}
